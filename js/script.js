3186470bd335ced05db35d00be7422293a4d8fff
Russell Rosario <rosariorussell@gmail.com>
2018-06-02 02:41:22 -0400

    added about section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-  $('#slides').superslides()
+  $('#slides').superslides({
+    animation: 'fade',
+    play: 5000,
+    pagination: false
+  })
+
+  var typed = new Typed('.typed', {
+    strings: ['Full Stack Web Developer', 'Financial Controller', 'Data Analyst', 'Ghostwriter'],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  })
 })
