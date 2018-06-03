d409d8b22c95d1dfa371741a0e29e5e6d66c6d6e
Russell Rosario <rosariorussell@gmail.com>
2018-06-03 16:47:50 -0400

    fixed portfolio for mobile responsiveness

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var mainPath = path.join(path.dirname(require.main.filename))
 var PORT = process.env.PORT || 3000
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-  res.sendFile(mainPath + '/public/main.html')
+app.use('', (req, res) => {
+  res.sendFile(mainPath + '/public/index.html')
 })
 
 app.listen(PORT, () => {
