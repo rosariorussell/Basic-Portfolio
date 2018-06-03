c799b8dcb2fbcc466db50d7db8067f5c77ef59cd
Russell Rosario <rosariorussell@gmail.com>
2018-06-03 00:06:28 -0400

    updated stats section

--- js/script.js
+++ js/script.js
@@ -33,6 +33,8 @@ $(document).ready(function () {
   })
 
   var skillsTopOffset = $('.skillsSection').offset().top
+  var statsTopOffset = $('.statsSection').offset().top
+  var countUpFinished = false
 
   $(window).scroll(function () {
     if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
@@ -47,6 +49,17 @@ $(document).ready(function () {
           $(this.el).find('.percent').text(Math.round(percent))
         }
       })
+
+      if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+        $('.counter').each(function () {
+          var element = $(this)
+          var endVal = parseInt(element.text())
+
+          element.countup(endVal)
+        })
+
+        countUpFinished = true
+      }
     }
   })
 })
