ca095fe42b10291199a60ea7e4ca6a1708d0ad32
Russell Rosario <rosariorussell@gmail.com>
2018-06-03 05:35:57 -0400

    completed portfolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+$(window).on('load', function () {
+  $('.loader .inner').fadeOut(500, function () {
+    $('.loader').fadeOut(750)
+  })
+})
+
 $(document).ready(function () {
   $('#slides').superslides({
     animation: 'fade',
@@ -56,6 +62,9 @@ $(document).ready(function () {
           var endVal = parseInt(element.text())
 
           element.countup(endVal)
+          if (endVal === 300) {
+            element.text('300k')
+          }
         })
 
         countUpFinished = true
@@ -90,4 +99,28 @@ $(document).ready(function () {
 
     return false
   })
+
+  $('#navigation li a').click(function (e) {
+    e.preventDefault()
+
+    var targetElement = $(this).attr('href')
+    var targetPosition = $(targetElement).offset().top
+    $('html, body').animate({ scrollTop: targetPosition - 50 }, 'slow')
+  })
+
+  const nav = $('#navigation')
+  const navTop = nav.offset().top
+
+  $(window).on('scroll', stickyNavigation)
+
+  function stickyNavigation () {
+    var body = $('body')
+    if ($(window).scrollTop() >= navTop) {
+      body.css('padding-top', nav.outerHeight() + 'px')
+      body.addClass('fixedNav')
+    } else {
+      body.css('padding-top', 0)
+      body.removeClass('fixedNav')
+    }
+  }
 })
